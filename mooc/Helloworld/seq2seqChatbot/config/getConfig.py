d7e4bc5120ffcbded9f0557f85fa322dee7ffc63
zhaoyingjun <>
2022-02-10 21:41:59 +0800

    hello world代码更新

---
+++ mooc/Helloworld/seq2seqChatbot/config/getConfig.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from configparser import SafeConfigParser
+config_file='config/seq2seq.ini'
+def get_config():
+    parser = SafeConfigParser()
+    parser.read(config_file)
+    # get the ints, floats and strings
+    _conf_ints = [ (key, int(value)) for key,value in parser.items('ints')]
+    #_conf_floats = [ (key, float(value)) for key,value in parser.items('floats') ]
+    _conf_strings = [ (key, str(value)) for key,value in parser.items('strings') ]
+    return dict(_conf_ints  + _conf_strings)
---
+++ mooc/Helloworld/seq2seqChatbot/data_util.py
@@ -0,0 +1,39 @@
+﻿# coding=utf-8
+import os
+import re
+import jieba
+from zhon.hanzi import punctuation
+from config import getConfig
+
+gConfig = {}
+gConfig= getConfig.get_config()
+conv_path = gConfig['resource_data']
+if not os.path.exists(conv_path):
+	print("找不到需要处理的文件，请确认在train_data文件中是否存在该文件")
+	exit()
+convs = []
+with open(conv_path,encoding='utf-8') as f:
+	one_conv = "" # 存储一次完整对话
+	for line in f:
+		line = line.strip('\n')
+		line=re.sub(r"[%s]+" %punctuation, "",line)#去除标点符号
+		if line == '':
+			continue
+		if line[0] == gConfig['e']:
+			if one_conv:
+
+				convs.append(one_conv[:-1])
+			one_conv = ""
+		elif line[0] == gConfig['m']:
+			one_conv=one_conv+str(" ".join(jieba.cut(line.split(' ')[1])))+'\t'#存储一次问或答
+
+
+seq_train = open(gConfig['seq_data'],'w')
+for i in range(len(convs)):
+   seq_train.write(convs[i]+'\n')
+   if i % 1000 == 0:
+      print(len(range(len(convs))), '处理进度：', i)
+seq_train.close()
+
+
+
