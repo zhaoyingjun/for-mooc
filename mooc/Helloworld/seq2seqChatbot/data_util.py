b5fe1e38e7c77b88200c6a0eece11a522900e4d9
zhaoyingjun <>
2022-03-01 00:38:09 +0800

    hello world代码更新

--- mooc/Helloworld/seq2seqChatbot/data_util.py
+++ mooc/Helloworld/seq2seqChatbot/data_util.py
@@ -12,8 +12,10 @@ if not os.path.exists(conv_path):
 	print("找不到需要处理的文件，请确认在train_data文件中是否存在该文件")
 	exit()
 convs = []
+seq_train = open(gConfig['seq_data'],'w')
 with open(conv_path,encoding='utf-8') as f:
 	one_conv = "" # 存储一次完整对话
+	i=0
 	for line in f:
 		line = line.strip('\n')
 		line=re.sub(r"[%s]+" %punctuation, "",line)#去除标点符号
@@ -21,19 +23,13 @@ with open(conv_path,encoding='utf-8') as f:
 			continue
 		if line[0] == gConfig['e']:
 			if one_conv:
-
-				convs.append(one_conv[:-1])
+				#@convs.append(one_conv[:-1])
+				seq_train.write(one_conv[:-1] + '\n')
+				i=i+1
+				if i % 1000 == 0:
+					print('处理进度：', i)
 			one_conv = ""
 		elif line[0] == gConfig['m']:
 			one_conv=one_conv+str(" ".join(jieba.cut(line.split(' ')[1])))+'\t'#存储一次问或答
 
-
-seq_train = open(gConfig['seq_data'],'w')
-for i in range(len(convs)):
-   seq_train.write(convs[i]+'\n')
-   if i % 1000 == 0:
-      print(len(range(len(convs))), '处理进度：', i)
 seq_train.close()
-
-
-
