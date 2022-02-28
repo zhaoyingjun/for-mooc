4697e6cd8a38c028d52c89eb84b6ba9df32ce5bf
zhaoyingjun <>
2022-03-01 00:20:49 +0800

    hello world代码更新

--- mooc/Helloworld/seq2seqChatbot/config/getConfig.py
+++ mooc/Helloworld/seq2seqChatbot/config/getConfig.py
@@ -4,7 +4,6 @@ from configparser import SafeConfigParser
 config_file=os.path.dirname(__file__)+'/seq2seq.ini'
 def get_config():
     parser = SafeConfigParser()
-    print(config_file)
     parser.read(config_file)
     # get the ints, floats and strings
     _conf_ints = [ (key, int(value)) for key,value in parser.items('ints')]
